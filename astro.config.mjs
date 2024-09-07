c5dc074cb6e2a3f0ccd301d2c627bb154d303e03
Pipo_Studio <67048178+pipodesignstudio@users.noreply.github.com>
2024-09-07 11:27:49 +0200

    cambios ssr cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,13 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-import node from '@astrojs/node';
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
   output: 'server',
-
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare(),
 });
